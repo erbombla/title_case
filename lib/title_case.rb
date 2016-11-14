43531b37a6cca6e1ac8fd8c061dc5c6e5d4a0b68
Eric Bomblatus and Margie Nevarez <margie@>
2016-11-14 15:30:38 -0800

    third spec and pass all tests

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -2,8 +2,13 @@ class String
   define_method(:title_case) do
     banana_split = self.split
     banana_split.each do |banana|
+      if banana === 'the'
+      # if banana === 'a' || banana === 'an' || banana === 'the' banana === 'or' || banana === 'for' || banana === 'on'
+      else
       banana.capitalize!()
     end
+    end
+    banana_split[0].capitalize!()
     banana_split.join(' ')
   end
 end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -6,6 +6,9 @@ describe('String#title_case') do
     expect(('rain').title_case()).to(eq('Rain'))
   end
   it('capitalizes the first letter of all words') do
-    expect(('have you ever seen the rain?').title_case()).to(eq('Have You Ever Seen The Rain?'))
+    expect(('the rain?').title_case()).to(eq('The Rain?'))
+  end
+  it('capitalizes the first letter of all words except prepositions, conjunctions, and articles') do
+    expect(('have you ever seen the rain?').title_case()).to(eq('Have You Ever Seen the Rain?'))
   end
 end
