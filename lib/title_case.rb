e6334e009dfb1d6c0d1cc8eb7ba1d11e72d4e14a
Eric Bomblatus <ebomblatus@gmail.com>
2016-11-15 22:51:47 -0800

    clean up code

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -5,12 +5,10 @@ class String
     sentence.each do |word|
       if exceptions.include?(word)
         word
-      # if banana === 'a' || banana === 'an' || banana === 'the' banana === 'or' || banana === 'for' || banana === 'on'
       else
         word.capitalize!()
       end
     end
-    # sentence[0].capitalize!()
     sentence.join(" ")
   end
 end
